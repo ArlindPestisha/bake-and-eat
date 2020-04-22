7ceea826ea6db4a87f2a1872b7c595243a5edff0
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-04-22 11:00:49 +0200

    Starting to build the functionality for the Search
    starting to put the state and functionality for the search methods.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 // Creating the state object where we controll the entire app in state
 /* *Global state
@@ -12,25 +14,28 @@ const state = {};
 // callback function for the controlSearch
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = 'pizza'
+    const query = searchView.getInput();
+    
 
     if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Preapre UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
 // Event listner for search form
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+// An object that contanic all the elemnts we need for our DOM
+
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
+};
