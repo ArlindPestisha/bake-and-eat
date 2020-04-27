8972e8ec04b81fac06821745461596d1a958e31a
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-04-27 15:29:00 +0200

    The base of UI stuff

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,5 +3,31 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
-    searchResList: document.querySelector('.results__list')
+    searchRes: document.querySelector('.results'),
+    searchResList: document.querySelector('.results__list'),
+    searchResPages: document.querySelector('.results__pages')
+};
+
+// New obj
+
+export const elementStrings = {
+    loader: 'loader'
+}
+
+// Creat loader
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>    
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+// Clear loader
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
 };
