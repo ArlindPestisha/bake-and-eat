59487f1137d6a25fb86dec1be6cbd9531be99f6f
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-05-01 13:04:50 +0200

    Going to creat new arr[]
    Going to creat new arr[] to fix things from the old and put everything in the same Uniform

--- src/js/models/recipe.js
+++ src/js/models/recipe.js
@@ -31,4 +31,28 @@ export default class Recipe {
   calcServings() {
       this.servings = 4;
   }
+
+  // Going to creat new arr[] to fix things from the old and put everything in the same Uniform
+  parseIngredients () {
+        // declaring to arr[] the old and the new wich is going to return from old arr
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+      //using the map() method to go throghu old arr and after that creat a new arr with newIngredients
+      const newIngredients = this.ingredients.map(el => {
+          // 1) Uniform units
+          let  ingredient = el.toLowerCase();
+          // Loop through the old arr[] and find the current el and convert to the new arr with the same index to Unifom elements
+          unitsLong.forEach((unit, i) => {
+              ingredient = ingredient.replace(unit, unitsShort[i])
+          });
+          // 2) Remove parentheses, using regular expression
+          ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+          // 3) Parse ingredients into count, unit and ingredients
+          return ingredient;
+      })
+      this.ingredients = newIngredients;
+
+  }
 }
