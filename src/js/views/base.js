baaba3a4151e08e25aaffaa13be4df440a30855f
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-05-02 12:18:08 +0200

    Import elements and recipe

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,11 +1,12 @@
-// An object that contanic all the elemnts we need for our DOM
+// An object that contanie all the elemnts we need for our DOM
 
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
     searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
-    searchResPages: document.querySelector('.results__pages')
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe')
 };
 
 // New obj
