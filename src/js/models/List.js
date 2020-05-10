bc672a0da8ea0e9688e69b964eedc4dbca066157
ArlindPestisha <arlind.pestisha@hyperisland.se>
2020-05-10 07:02:57 +0200

    Create list

---
+++ src/js/models/List.js
@@ -0,0 +1,32 @@
+import uniqid from 'uniqid'
+
+// Creat a class 
+export default class List {
+    constructor() {
+        //creating new empty list
+        this.items = []
+    }
+
+    // Creat a method to add new items to the list
+    addItem (count, unit, ingredient) {
+        const item = {
+            id:uniqid(),
+            count,
+            unit,
+            ingredient
+        }
+        this.items.push(item)
+    return item
+    }
+
+    deleteItem(id) {
+        const index = this.items.findIndex(el => el.id === id)
+        // [2,4,8] splice(1, 1) -> returns 4, original array is [2, 8]
+        // [2,4,8] slice(1, 2) -> returns 4, original array is [2, 4, 8] 
+        this.items.splice(index, 1)
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount
+    }
+}
